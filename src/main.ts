effafc8445242bf7b16221a49eb9820318ac65f3
sina <spsina>
2023-05-02 20:48:27 +0330

    WIP: parent and referral

--- src/main.ts
+++ src/main.ts
@@ -1,171 +1,25 @@
-import { BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
-import { PairFactory } from "../generated/Router/PairFactory";
-import { ERC20 } from "../generated/Router/ERC20";
-import { Router, Swap } from "../generated/Router/Router";
+import { Address } from "@graphprotocol/graph-ts";
+import { RequestHatch } from "../generated/DegenZoo/DegenZoo";
+import { Referral, RequestHatchLog } from "../generated/schema";
+import { getOrCreateReferral } from "./utils";
 
-import {
-  ZERO_ADDRESS,
-  addAccumulativeTokenBalance,
-  getOrCreateGeneratedVolume,
-  createReferral,
-  createSwapLog,
-  getDIBS as getDibs,
-  getOrCreateLottery,
-  getOrCreateUserLottery,
-  getDIBSLottery as getDibsLottery,
-  getBNBChainLink as getPriceFeed,
-  updateVolume,
-  VolumeType,
-  getRewardPercentage,
-  getNumberOfTickets,
-  getOrCreateWeeklyGeneratedVolume,
-  getRoutes,
-} from "./utils";
-import { Dibs } from "../generated/Router/Dibs";
-import { DibsLottery } from "../generated/Router/DibsLottery";
-import { EACAggregatorProxy } from "../generated/Router/EACAggregatorProxy";
-
-export function handleSwap(event: Swap): void {
-  // extract swap params from event
-  const token = event.params._tokenIn;
-  const user = event.params.sender;
-  const amount = event.params.amount0In;
-  const timestamp = event.block.timestamp;
-  const routerAddress = event.address;
-
-  const router = Router.bind(routerAddress);
-  const pairFactory = PairFactory.bind(router.factory());
-  const dibs = Dibs.bind(pairFactory.dibs());
-  const dibsLottery = DibsLottery.bind(dibs.dibsLottery());
-  const wethPriceFeed = EACAggregatorProxy.bind(dibs.wethPriceFeed());
-
-  const inputToken = ERC20.bind(token);
-
-  const round = dibsLottery.getActiveLotteryRound();
-
-  // check if user is not registered in the dibs contract
-  // then return
-  const userCode = dibs.addressToCode(user);
-  if (userCode == Bytes.empty()) {
-    return;
-  }
-
-  // since all registered users have a parent,
-  // we can get the parent and grandparent address
-  const parentAddress = dibs.parents(user);
-  let grandParentAddress = dibs.parents(parentAddress);
-
-  if (parentAddress == ZERO_ADDRESS) {
-    return;
-  }
-
-  // if the grandparent address is address 0x0, set grandparent address to dibs address
-  if (grandParentAddress == ZERO_ADDRESS) {
-    grandParentAddress = dibs.codeToAddress(dibs.DIBS());
-  }
-
-  // get volume in BNB
-  let volumeInWeth: BigInt;
-
-  const precision = 4;
-  const weth = router.wETH();
-
-  if (token == weth) {
-    // if input token is wETH, no need to make conversions
-    volumeInWeth = amount;
-  } else {
-    // in case input token is not wETH
-    const unit = BigInt.fromI32(10).pow(u8(inputToken.decimals() - precision));
-    let unitVolumeInWeth: BigInt;
-    const routeToWeth = getRoutes(token, weth);
-    if (routeToWeth.length > 0) {
-      // if there is a rout from input token to wETH
-      unitVolumeInWeth = router
-        .getAmountsOut(
-          unit, // 0.0001 unit of the input token
-          routeToWeth
-        )
-        .pop(); // last price
-    } else {
-      // no route to wETH
-      unitVolumeInWeth = BigInt.fromI32(0);
-    }
-    volumeInWeth = unitVolumeInWeth.times(amount).div(unit); // time the amount of input token
-  }
-
-  const volumeInDollars = wethPriceFeed
-    .latestAnswer()
-    .times(volumeInWeth)
-    .div(BigInt.fromI32(10).pow(u8(wethPriceFeed.decimals())));
-
-  // update generated volume for user, parent and grandparent
-  updateVolume(user, volumeInDollars, timestamp, VolumeType.USER);
-  updateVolume(parentAddress, volumeInDollars, timestamp, VolumeType.PARENT);
-  updateVolume(
-    grandParentAddress,
-    volumeInDollars,
-    timestamp,
-    VolumeType.GRANDPARENT
+export function handleRequestHatch(event: RequestHatch): void {
+  const rhl = new RequestHatchLog(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
 
-  // calculate total amount of reward based on MAX_REFERRAL_FEE from pairFactory
-  const feeScale = 10000;
-  const feeRate = pairFactory.getFee(event.params.stable);
-  const feeAmount = amount.times(feeRate).div(BigInt.fromI32(feeScale));
-  const rewardPercentage = getRewardPercentage(
-    getOrCreateGeneratedVolume(parentAddress).amountAsReferrer
-  );
-  const rewardAmount = feeAmount
-    .times(rewardPercentage)
-    .div(BigInt.fromI32(10000));
+  rhl.txHash = event.transaction.hash;
+  rhl.user = event.params.who;
+  rhl.tokenId = event.params.tokenId;
+  rhl.requestId = event.params.requestId;
+  rhl.amount = event.params.amount;
+  rhl.referrer = event.params.referral;
+  rhl.timestamp = event.block.timestamp;
+  rhl.save();
 
-  // calculate the amount of tokens that the parent and grandparent and dibs platform will receive
-  const scale = dibs.SCALE();
-  const grandParentPercentage = dibs.grandparentPercentage();
-  const dibsPercentage = dibs.dibsPercentage();
-  const grandParentAmount = rewardAmount
-    .times(grandParentPercentage)
-    .div(scale);
-  const dibsAmount = rewardAmount.times(dibsPercentage).div(scale);
-  const parentAmount = rewardAmount.minus(grandParentAmount.plus(dibsAmount));
+  const referral = getOrCreateReferral(rhl.referrer, rhl.user);
 
-  // add the reward amount to the accumulative token balance for the parent, grandparent and dibs platform
-  addAccumulativeTokenBalance(token, parentAddress, parentAmount, timestamp);
-  addAccumulativeTokenBalance(
-    token,
-    grandParentAddress,
-    grandParentAmount,
-    timestamp
-  );
-  addAccumulativeTokenBalance(
-    token,
-    dibs.codeToAddress(dibs.DIBS()),
-    dibsAmount,
-    timestamp
-  );
+  const parent = referral.referrer;
 
-  // create a referral if it does not exist
-  createReferral(parentAddress, user);
-  createSwapLog(
-    event,
-    round,
-    volumeInWeth,
-    wethPriceFeed.latestAnswer(),
-    volumeInDollars
-  );
-
-  const lottery = getOrCreateLottery(round);
-  const userLottery = getOrCreateUserLottery(round, user);
-  const tickets = getNumberOfTickets(
-    getOrCreateWeeklyGeneratedVolume(user, round).amountAsUser
-  );
-
-  const addedTickets = tickets.minus(userLottery.tickets);
-
-  if (addedTickets.gt(BigInt.fromI32(0))) {
-    userLottery.tickets = tickets;
-    lottery.totalTickets = lottery.totalTickets.plus(addedTickets);
-    userLottery.save();
-    lottery.save();
-  }
+  const parentReferral = Referral.load(parent.toHex());
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,39 +1,23 @@
-import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+
 import {
-  Router__getAmountsOutInputRoutesStruct,
-  Swap,
-} from "../generated/Router/Router";
-import {
-  AccumulativeTokenBalance,
   Referral,
   GeneratedVolume,
-  SwapLog,
   Lottery,
   UserLottery,
   AccumulativeGeneratedVolume,
   WeeklyGeneratedVolume,
   DailyGeneratedVolume,
-  PathToTarget,
-  Pair,
 } from "../generated/schema";
-import { Dibs } from "../generated/Router/Dibs";
-import { DibsLottery } from "../generated/Router/DibsLottery";
-import { EACAggregatorProxy } from "../generated/Router/EACAggregatorProxy";
+import { Dibs } from "../generated/DegenZoo/Dibs";
+import { Pair } from "../generated/DegenZoo/Pair";
+import { ERC20 } from "../generated/DegenZoo/ERC20";
+import { DibsLottery } from "../generated/DegenZoo/DibsLottery";
+import { EACAggregatorProxy } from "../generated/DegenZoo/EACAggregatorProxy";
 
 export const ZERO_ADDRESS = Address.fromHexString(
   "0x0000000000000000000000000000000000000000"
 );
-
-export const EPOCH_START_TIMESTAMP = BigInt.fromI32(1673481600);
-export const EPOCH_LENGTH = BigInt.fromI32(604800);
-
-export function getRound(timestamp: BigInt): BigInt {
-  return timestamp.minus(EPOCH_START_TIMESTAMP).div(EPOCH_LENGTH);
-}
-
-export function getDay(timestamp: BigInt): BigInt {
-  return timestamp.minus(EPOCH_START_TIMESTAMP).div(BigInt.fromI32(86400));
-}
 
 export enum VolumeType {
   USER,
@@ -41,26 +25,14 @@ export enum VolumeType {
   GRANDPARENT,
 }
 
-export function addAccumulativeTokenBalance(
-  token: Address,
-  user: Address,
-  amount: BigInt,
-  timestamp: BigInt
-): void {
-  let id = token.toHex() + "-" + user.toHex();
-  let accumulativeTokenBalance = AccumulativeTokenBalance.load(id);
-  if (accumulativeTokenBalance == null) {
-    accumulativeTokenBalance = new AccumulativeTokenBalance(id);
-    accumulativeTokenBalance.token = token;
-    accumulativeTokenBalance.user = user;
-    accumulativeTokenBalance.amount = BigInt.fromI32(0);
-  }
-  accumulativeTokenBalance.amount = accumulativeTokenBalance.amount.plus(
-    amount
-  );
-  accumulativeTokenBalance.lastUpdate = timestamp;
-  accumulativeTokenBalance.save();
+export function getRound(): BigInt {
+  return DibsLottery.bind(getDibs().dibsLottery()).getActiveLotteryRound();
 }
+
+export function getDay(): BigInt {
+  return DibsLottery.bind(getDibs().dibsLottery()).getActiveDay();
+}
+
 export function getOrCreateGeneratedVolume(user: Address): GeneratedVolume {
   let id = user.toHex();
   let generatedVolume = GeneratedVolume.load(id);
@@ -133,11 +105,11 @@ export function updateVolume(
   const generatedVolume = getOrCreateGeneratedVolume(user);
   const accWeeklyGeneratedVolume = getOrCreateWeeklyGeneratedVolume(
     user,
-    getRound(timestamp)
+    getRound()
   );
   const accDailyGeneratedVolume = getOrCreateDailyGeneratedVolume(
     user,
-    getDay(timestamp)
+    getDay()
   );
   if (volumeType == VolumeType.USER) {
     generatedVolume.amountAsUser = generatedVolume.amountAsUser.plus(amount);
@@ -205,8 +177,11 @@ export function getOrCreateUserLottery(
   }
   return userLottery;
 }
-export function createReferral(referrer: Address, user: Address): void {
-  let id = user.toHex() + "-" + referrer.toHex();
+export function getOrCreateReferral(
+  referrer: Address,
+  user: Address
+): Referral {
+  let id = user.toHex();
   let referral = Referral.load(id);
   if (referral == null) {
     referral = new Referral(id);
@@ -214,96 +189,43 @@ export function createReferral(referrer: Address, user: Address): void {
     referral.referrer = referrer;
     referral.save();
   }
-}
-export function createSwapLog(
-  event: Swap,
-  lotteryRound: BigInt,
-  volumeInBNB: BigInt,
-  BNBPrice: BigInt,
-  volumeInDollars: BigInt
-): void {
-  // log the swap itself
-  let swap = new SwapLog(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  );
-  swap.txHash = event.transaction.hash;
-  swap.logIndex = event.logIndex;
-  swap.user = event.params.sender;
-  swap.tokenIn = event.params._tokenIn;
-  swap.amountIn = event.params.amount0In;
-  swap.volumeInBNB = volumeInBNB;
-  swap.BNBPrice = BNBPrice;
-  swap.volumeInDollars = volumeInDollars;
-  swap.round = lotteryRound;
-  swap.stable = event.params.stable;
-  swap.timestamp = event.block.timestamp;
-  swap.save();
+  return referral;
 }
 
-export function getBNBChainLink(): EACAggregatorProxy {
+export function getDzooPrice(): BigInt {
+  const pair = Pair.bind(
+    Address.fromString("0xd6251Ba6Af2002588f89D3c599A9929f6b0F6A99")
+  );
+
+  const reserves = pair.getReserves();
+
+  const wbnbPerDzoo = reserves.value1
+    .times(e18(BigInt.fromI32(1)))
+    .div(reserves.value0);
+
+  const dzooPrice = getBnbChainLink()
+    .latestAnswer()
+    .times(wbnbPerDzoo)
+    .div(BigInt.fromI32(10).pow(18 + getBnbChainLink().decimals()));
+
+  return dzooPrice;
+}
+
+export function getBnbChainLink(): EACAggregatorProxy {
   return EACAggregatorProxy.bind(
     Address.fromString("0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE")
   );
 }
 
-export function getDIBS(): Dibs {
+export function getDibs(): Dibs {
   return Dibs.bind(
     Address.fromString("0x664cE330511653cB2744b8eD50DbA31C6c4C08ca")
-  );
-}
-
-export function getDIBSLottery(): DibsLottery {
-  return DibsLottery.bind(
-    Address.fromString("0x287ed50e4c158dac38e1b7e16c50cd1b2551a300")
   );
 }
 
 function e18(amount: BigInt): BigInt {
   const E18 = BigInt.fromI32(10).pow(18);
   return amount.times(E18);
-}
-
-export function getRoutes(
-  token: Address,
-  target: Address
-): Array<Router__getAmountsOutInputRoutesStruct> {
-  const pathToTarget = PathToTarget.load(token.toHex() + "-" + target.toHex());
-  if (pathToTarget == null) {
-    return [];
-  } else {
-    const routes: Array<Router__getAmountsOutInputRoutesStruct> = new Array();
-    let inToken = token;
-    for (let i = 0; i < pathToTarget.path.length; i++) {
-      const pairAddress = pathToTarget.path[i];
-      const pair = Pair.load(pairAddress.toHex())!;
-      const otherToken = inToken.equals(pair.token0)
-        ? Address.fromBytes(pair.token1)
-        : Address.fromBytes(pair.token0);
-
-      const route: Router__getAmountsOutInputRoutesStruct = new Router__getAmountsOutInputRoutesStruct();
-      route.push(ethereum.Value.fromAddress(inToken));
-      route.push(ethereum.Value.fromAddress(otherToken));
-      route.push(ethereum.Value.fromBoolean(pair.stable));
-      routes.push(route);
-
-      inToken = otherToken;
-    }
-    return routes;
-  }
-}
-
-export function getRewardPercentage(volume: BigInt): BigInt {
-  if (volume <= e18(BigInt.fromString("30000"))) {
-    return BigInt.fromI32(500);
-  } else if (volume <= e18(BigInt.fromString("150000"))) {
-    return BigInt.fromI32(650);
-  } else if (volume <= e18(BigInt.fromString("1000000"))) {
-    return BigInt.fromI32(800);
-  } else if (volume <= e18(BigInt.fromString("10000000"))) {
-    return BigInt.fromI32(1000);
-  } else {
-    return BigInt.fromI32(1200);
-  }
 }
 
 export function getNumberOfTickets(volume: BigInt): BigInt {
