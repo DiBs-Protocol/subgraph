f1042fc24d91cf50da4142a2532c9390b537fbd3
Ali Parvizi <snparvizi75@gmail.com>
2023-10-17 17:09:15 +0200

    adjust based starttimestamp

--- src/symm/SymmLiquidatePositionsPartyAHandler.ts
+++ src/symm/SymmLiquidatePositionsPartyAHandler.ts
@@ -1,7 +1,10 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts"
 import { LiquidatePositionsPartyA, v3 } from "../../generated/SymmDataSource/v3"
 
-import { EPOCH_START_TIMESTAMP, MULTI_ACCOUNT_ADDRESS } from "../../config/config"
+import {
+  EPOCH_START_TIMESTAMP,
+  MULTI_ACCOUNT_ADDRESS
+} from "../../config/config"
 import { Quote } from "../../generated/schema"
 import { zero_address } from "../solidly/utils"
 
@@ -34,7 +37,8 @@ export class LiquidatePositionsPartyAHandler {
     updateVolume(
       Address.fromBytes(zero_address),
       this.day,
-      volumeInDollars, this.timestamp
+      volumeInDollars,
+      this.timestamp
     ) // total volume tracker
   }
 
@@ -43,20 +47,16 @@ export class LiquidatePositionsPartyAHandler {
     let symmioContract = v3.bind(this.event.address)
 
     const callResult = symmioContract.try_getQuote(quoteId)
-    if (callResult.reverted)
-      return BigInt.zero() //FIXME
+    if (callResult.reverted) return BigInt.zero() //FIXME
     let chainQuote = callResult.value
     const liquidAmount = quote.quantity.minus(quote.closedAmount)
     const liquidPrice = chainQuote.avgClosedPrice
       .times(quote.quantity)
-      .minus(
-        quote.avgClosedPrice
-          .times(quote.closedAmount)
-      )
+      .minus(quote.avgClosedPrice.times(quote.closedAmount))
       .div(liquidAmount)
     return liquidAmount
       .times(liquidPrice)
-      .times(BigInt.fromI32(4))
+      .times(BigInt.fromI32(1))
       .div(BigInt.fromString("10").pow(18))
   }
 }
