7a18fc772ad1614c2b585fc56f9b8345390c08c4
Ali Parvizi <snparvizi75@gmail.com>
2024-01-02 19:50:26 +0100

    update interface

--- src/symm/Handler.ts
+++ src/symm/Handler.ts
@@ -3,7 +3,7 @@ import {
   EPOCH_START_TIMESTAMP,
   MULTI_ACCOUNT_ADDRESS,
   OLD_MULTI_ACCOUNT_ADDRESS,
-  OLD_SYMMIO_ADDRESS
+  OLD_SYMMIO_ADDRESS,
 } from "../../config/config"
 import { MultiAccount_old } from "../../generated/SymmDataSourceOld/MultiAccount_old"
 import { MultiAccount } from "../../generated/SymmDataSource/MultiAccount"
@@ -14,12 +14,11 @@ export class Handler {
   timestamp: BigInt
   day: BigInt
 
-
   constructor(event: ethereum.Event) {
     this.timestamp = event.block.timestamp
     this._event = event
     const delta = event.block.timestamp.minus(
-      BigInt.fromI32(EPOCH_START_TIMESTAMP)
+      BigInt.fromI32(EPOCH_START_TIMESTAMP),
     )
 
     this.isValid = delta.ge(BigInt.fromI32(0))
@@ -33,16 +32,12 @@ export class Handler {
   }
 
   public getOwner(account: Address): Address {
-    if (this._event.address.toHexString() == OLD_SYMMIO_ADDRESS) {
-      const multiAccount = MultiAccount_old.bind(
-        Address.fromString(OLD_MULTI_ACCOUNT_ADDRESS)
-      )
-      return multiAccount.owner(account)
-    } else {
-      const multiAccount = MultiAccount.bind(
-        Address.fromString(MULTI_ACCOUNT_ADDRESS)
-      )
-      return multiAccount.owners(account)
-    }
+    const multiAccount = MultiAccount.bind(
+      Address.fromString(MULTI_ACCOUNT_ADDRESS),
+    )
+
+    const owner = multiAccount.owners(account)
+
+    return owner
   }
 }
